b3e0dfb1003338e2770f84a1e85effee471e3f86
Ömer Tuğrul <omertugrul24@gmail.com>
2022-07-05 22:10:55 +0300

    Create full_adder.vhd

---
+++ project7/full_adder.vhd
@@ -0,0 +1,23 @@
+-- Full adder for Adder module
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fulladder is
+	port(
+		x : in std_logic;
+		y : in std_logic;
+		cin: in std_logic;
+		--
+		sum : out std_logic;
+		cout: out std_logic	
+	);
+end fulladder;
+
+architecture rtl of fulladder is
+begin
+
+sum <= x xor y xor cin;
+cout<= (x and y)or (x and cin) or (y and cin);
+
+end architecture;
